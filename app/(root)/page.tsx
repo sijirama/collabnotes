787aed4b8f6aebc9c6004ef4226e19e3257817f7
sijirama <gbemilesanmi@gmail.com>
2024-08-01 16:36:08 +0100

    okay now

--- app/(root)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
-
-export default function Home() {
-    return (
-        <div><Button>Click here</Button></div>
-    )
-}
-
